cc3bb6347b5fe538b8e1caf31c7687b874bf1062
lijune-risd <90545980+lijune-risd@users.noreply.github.com>
2022-09-27 22:16:19 -0400

    Add files via upload

---
+++ assets/data.js
@@ -0,0 +1,53 @@
+export const data = [
+	{
+		title: `Design Bookstore`,
+		image: './assets/images/house_tr.png',
+		color: '',
+		description: `Design Bookstore ipsum dolor sit amet consectetur adipisicing elit. Ratione, officiis alias amet ab asperiores, tempora id nihil nam molestiae fugiat magnam commodi. Laboriosam sequi facilis tenetur temporibus deleniti enim ipsum, amet ipsa accusantium eum numquam iste laudantium maxime. Rerum facere eius blanditiis perspiciatis officia voluptates provident. Est velit aliquam vero iusto vel atque, ratione, reiciendis assumenda asperiores harum blanditiis inventore quidem nam non sint natus ipsam voluptate tenetur!`
+	},
+	{
+		title: `Seoul Design Festa`,
+		image: './assets/images/house_tr.png',
+		color: '',
+		description: `
+			<p>Seoul Design Festa ipsum dolor sit amet consectetur adipisicing elit. Ratione, officiis alias amet ab asperiores, tempora id nihil nam molestiae fugiat magnam commodi. Laboriosam sequi facilis tenetur temporibus deleniti enim ipsum, amet ipsa accusantium eum numquam iste laudantium maxime. Rerum facere eius blanditiis perspiciatis officia voluptates provident.</p>
+			<p>Est velit aliquam vero iusto vel atque, ratione, reiciendis assumenda asperiores harum blanditiis inventore quidem nam non sint natus ipsam voluptate tenetur!</p>`
+	},
+	{
+		title: `Lorem ipsum`,
+		image: './assets/images/house_tr.png',
+		color: '',
+		description: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione, officiis alias amet ab asperiores, tempora id nihil nam molestiae fugiat magnam commodi. Laboriosam sequi facilis tenetur temporibus deleniti enim ipsum, amet ipsa accusantium eum numquam iste laudantium maxime. Rerum facere eius blanditiis perspiciatis officia voluptates provident. Est velit aliquam vero iusto vel atque, ratione, reiciendis assumenda asperiores harum blanditiis inventore quidem nam non sint natus ipsam voluptate tenetur!`
+	},
+	{
+		title: `Tina's Portfolio`,
+		image: './assets/images/house_tr.png',
+		color: '',
+		description: `Tina's Portfolio ipsum dolor sit amet consectetur adipisicing elit. Ratione, officiis alias amet ab asperiores, tempora id nihil nam molestiae fugiat magnam commodi. Laboriosam sequi facilis tenetur temporibus deleniti enim ipsum, amet ipsa accusantium eum numquam iste laudantium maxime. Rerum facere eius blanditiis perspiciatis officia voluptates provident. Est velit aliquam vero iusto vel atque, ratione, reiciendis assumenda asperiores harum blanditiis inventore quidem nam non sint natus ipsam voluptate tenetur!`
+	},
+	{
+		title: 'Butter',
+		image: './assets/images/house_tr.png',
+		color: '',
+		description: `Butter ipsum dolor sit amet consectetur adipisicing elit. Ratione, officiis alias amet ab asperiores, tempora id nihil nam molestiae fugiat magnam commodi. Laboriosam sequi facilis tenetur temporibus deleniti enim ipsum, amet ipsa accusantium eum numquam iste laudantium maxime. Rerum facere eius blanditiis perspiciatis officia voluptates provident. Est velit aliquam vero iusto vel atque, ratione, reiciendis assumenda asperiores harum blanditiis inventore quidem nam non sint natus ipsam voluptate tenetur!`
+	},
+	{
+		title: 'Colo Company',
+		image: './assets/images/house_tr.png',
+		color: '',
+		description: `Colo Company ipsum dolor sit amet consectetur adipisicing elit. Ratione, officiis alias amet ab asperiores, tempora id nihil nam molestiae fugiat magnam commodi. Laboriosam sequi facilis tenetur temporibus deleniti enim ipsum, amet ipsa accusantium eum numquam iste laudantium maxime. Rerum facere eius blanditiis perspiciatis officia voluptates provident. Est velit aliquam vero iusto vel atque, ratione, reiciendis assumenda asperiores harum blanditiis inventore quidem nam non sint natus ipsam voluptate tenetur!`
+	},
+	{
+		title: 'Jeju Island',
+		image: './assets/images/house_tr.png',
+		color: '',
+		description: `Jeju Island ipsum dolor sit amet consectetur adipisicing elit. Ratione, officiis alias amet ab asperiores, tempora id nihil nam molestiae fugiat magnam commodi. Laboriosam sequi facilis tenetur temporibus deleniti enim ipsum, amet ipsa accusantium eum numquam iste laudantium maxime. Rerum facere eius blanditiis perspiciatis officia voluptates provident. Est velit aliquam vero iusto vel atque, ratione, reiciendis assumenda asperiores harum blanditiis inventore quidem nam non sint natus ipsam voluptate tenetur!`
+	},
+	{
+		title: 'Goooogle',
+		image: './assets/images/house_tr.png',
+		color: '',
+		description: `Goooogle ipsum dolor sit amet consectetur adipisicing elit. Ratione, officiis alias amet ab asperiores, tempora id nihil nam molestiae fugiat magnam commodi. Laboriosam sequi facilis tenetur temporibus deleniti enim ipsum, amet ipsa accusantium eum numquam iste laudantium maxime. Rerum facere eius blanditiis perspiciatis officia voluptates provident. Est velit aliquam vero iusto vel atque, ratione, reiciendis assumenda asperiores harum blanditiis inventore quidem nam non sint natus ipsam voluptate tenetur!`
+	},
+];
+
